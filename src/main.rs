bddbcaeb4761050ec5046393d18d6fbf3fd1b91f
Daiki Mizukami <mizukami1113@gmail.com>
2017-05-08 15:00:03 +0900

    Implement automatic saving feature

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ use futures::{Future, Stream};
 use json::Value;
 use std::collections::BTreeMap;
 use std::fs::{File, OpenOptions};
-use std::io::{BufReader, BufWriter};
+use std::io::{self, BufReader, BufWriter, Seek, SeekFrom, Write};
 use std::ops::Not;
 use tokio_core::reactor::Core;
 use twitter_stream::TwitterStream;
@@ -56,41 +56,75 @@ impl TypeSet {
         TypeSet::default()
     }
 
-    fn add_type_of(&mut self, v: Value) {
+    fn add_type_of(&mut self, v: Value) -> bool {
         match v {
-            Value::Null => self.null = true,
-            Value::Bool(_) => self.bool = true,
-            Value::Number(_) => self.number = true,
-            Value::String(_) => self.string = true,
+            Value::Null => {
+                let changes = ! self.null;
+                self.null = true;
+                changes
+            },
+            Value::Bool(_) => {
+                let changes = ! self.bool;
+                self.bool = true;
+                changes
+            },
+            Value::Number(_) => {
+                let changes = ! self.number;
+                self.number = true;
+                changes
+            },
+            Value::String(_) => {
+                let changes = ! self.string;
+                self.string = true;
+                changes
+            },
             Value::Array(a) => {
                 let mut types = self.array.take().unwrap_or_else(Default::default);
 
-                if a.is_empty() {
+                let changed = if a.is_empty() {
+                    let changes = ! types.absent;
                     types.absent = true;
+                    changes
                 } else {
-                    for v in a {
-                        types.add_type_of(v);
-                    }
-                }
+                    a.into_iter().map(|v| {
+                        types.add_type_of(v)
+                    }).any(id)
+                };
 
                 self.array = Some(types);
+
+                changed
             },
             Value::Object(m) => {
                 let mut map = self.object.take().unwrap_or_else(Map::new);
 
-                for (k, types) in &mut map {
+                let changed = map.iter_mut().map(|(k, types)| {
                     if ! m.contains_key(k) {
+                        let changes = ! types.absent;
                         types.absent = true;
+                        changes
+                    } else {
+                        false
                     }
-                }
+                }).any(id);
 
-                for (k, v) in m {
-                    map.entry(k).or_insert_with(TypeSet::new).add_type_of(v);
-                }
+                let changed = changed || m.into_iter().map(|(k, v)| {
+                    map.entry(k).or_insert_with(TypeSet::new).add_type_of(v)
+                }).any(id);
 
                 self.object = Some(map);
+
+                changed
             },
         }
+    }
+
+    fn write_to_file(&self, mut f: &mut BufWriter<File>) -> io::Result<()> {
+        f.seek(SeekFrom::Start(0))?;
+        f.get_ref().set_len(0)?;
+        json::to_writer_pretty(&mut f, self)
+            .map_err(|e| io::Error::new(io::ErrorKind::Other, e))?;
+        f.flush()
     }
 }
 
@@ -119,30 +153,41 @@ fn main() {
         .and_then(|f| json::from_reader(f).ok())
         .unwrap_or_else(TypeSet::new);
 
+    let output = matches.value_of("output")
+        .or_else(|| matches.value_of("resume"))
+        .unwrap_or("types.json");
+    let mut output = OpenOptions::new()
+            .write(true)
+            .create(true)
+            .open(output)
+            .map(BufWriter::new).unwrap();
+
     let mut core = Core::new().unwrap();
     let handle = core.handle();
 
     {
         let fut = TwitterStream::sample(&token, &handle).flatten_stream().for_each(|json| {
             let json = json::from_str(&json).map_err(twitter_stream::Error::custom)?;
-            root.add_type_of(json);
+            let changed = root.add_type_of(json);
+
+            if changed {
+                print!("writing to the output file... ");;
+                root.write_to_file(&mut output).unwrap();
+                println!("ok");
+            }
+
             Ok(())
         });
 
+        // Catch Ctrl-C to ensure graceful shutdown.
         let ctrl_c = tokio_signal::ctrl_c(&handle).flatten_stream().into_future();
 
         assert!(core.run(fut.select2(ctrl_c)).is_ok());
     }
 
-    let f = matches.value_of("output")
-        .or_else(|| matches.value_of("resume"))
-        .unwrap_or("types.json");
-    let f = OpenOptions::new()
-        .write(true)
-        .create(true)
-        .truncate(true)
-        .open(f)
-        .map(BufWriter::new).unwrap();
+    root.write_to_file(&mut output).unwrap();
+}
 
-    json::to_writer_pretty(f, &root).unwrap();
+fn id<T>(t: T) -> T {
+    t
 }
